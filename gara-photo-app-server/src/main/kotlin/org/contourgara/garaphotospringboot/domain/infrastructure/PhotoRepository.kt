a1b10504a4249fcc0344cf950443830e300d7a3d
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-04-15 04:46:23 +0900

    sonarqube の指摘を対応

--- gara-photo-app-server/src/main/kotlin/org/contourgara/garaphotospringboot/domain/infrastructure/PhotoRepository.kt
+++ gara-photo-app-server/src/main/kotlin/org/contourgara/garaphotospringboot/domain/infrastructure/PhotoRepository.kt
@@ -2,6 +2,6 @@ package org.contourgara.garaphotospringboot.domain.infrastructure
 
 import org.contourgara.garaphotospringboot.domain.Media
 
-interface PhotoRepository {
+fun interface PhotoRepository {
   fun findForYesterday(sourcePath: String): Media
 }
--- gara-photo-app-server/src/main/kotlin/org/contourgara/garaphotospringboot/domain/infrastructure/TwitterClient.kt
+++ gara-photo-app-server/src/main/kotlin/org/contourgara/garaphotospringboot/domain/infrastructure/TwitterClient.kt
@@ -2,6 +2,6 @@ package org.contourgara.garaphotospringboot.domain.infrastructure
 
 import org.contourgara.garaphotospringboot.domain.Tweet
 
-interface TwitterClient {
+fun interface TwitterClient {
   fun tweet(tweet: Tweet, accessToken: String): Long
 }
