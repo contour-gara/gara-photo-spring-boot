b61dcd55cf4993219fcdcdb04557e22f1adb1d43
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-05-24 02:12:56 +0900

    全ての結合テストを実装

--- integration-test/src/test/kotlin/org/contourgara/integration/GaraPhotoSpringBootIT.kt
+++ integration-test/src/test/kotlin/org/contourgara/integration/GaraPhotoSpringBootIT.kt
@@ -21,7 +21,6 @@ import java.sql.DriverManager
 @SpringBootTest
 @DBRider
 @DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
-@WireMockTest(httpPort = 28080)
 class GaraPhotoSpringBootIT {
   companion object {
     private const val DB_URL: String = "jdbc:postgresql://localhost:5432/gara_photo"
@@ -39,6 +38,7 @@ class GaraPhotoSpringBootIT {
     }
   }
 
+  @WireMockTest(httpPort = 28080)
   @Nested
   inner class `OAuth エンドポイント` {
     @Test
@@ -75,6 +75,74 @@ class GaraPhotoSpringBootIT {
         .post("/v1/oauth/token")
         .then()
         .statusCode(HttpStatus.NO_CONTENT.value())
+    }
+
+    @Nested
+    inner class `アクセストークンを取得するためのエンドポイントを GET した場合` {
+      @Test
+      @DataSet(value = ["datasets/setup/1-token.yml"])
+      @ExpectedDataSet(value = ["datasets/expected/1-token.yml"])
+      fun `レスポンスコード 200 とアクセストークンが返る`() {
+        // execute & assert
+        given()
+          .get("/v1/oauth/token")
+          .then()
+          .statusCode(HttpStatus.OK.value())
+          .body("accessToken", Matchers.equalTo("access-token"))
+      }
+      @Test
+      @DataSet(value = ["datasets/setup/1-token-old.yml"])
+      @ExpectedDataSet(value = ["datasets/expected/1-token-update.yml"])
+      fun `かつトークン取得が古い場合、レスポンスコード 200 と新しいアクセストークンが返る`() {
+        // setup
+        stubFor(post(urlEqualTo("/oauth2/token"))
+          .withHeader("Content-Type", equalTo("application/x-www-form-urlencoded"))
+          .withHeader("Authorization", equalTo("Basic Y2xpZW50LWlkLWR1bW15OmNsaWVudC1zZWNyZXQtZHVtbXk="))
+          .withRequestBody(equalTo("grant_type=refresh_token&refresh_token=refresh-token&client_id=client-id-dummy"))
+          .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", "application/json; charset=utf-8")
+            .withBody("{\"token_type\":\"bearer\",\"expires_in\":7200,\"access_token\":\"access-token2\",\"scope\":\"tweet.write users.read tweet.read offline.access\",\"refresh_token\":\"refresh-token2\"}")))
+
+        // execute & assert
+        given()
+          .get("/v1/oauth/token")
+          .then()
+          .statusCode(HttpStatus.OK.value())
+          .body("accessToken", Matchers.equalTo("access-token2"))
+      }
+    }
+  }
+
+  @WireMockTest(httpPort = 28080)
+  @Nested
+  inner class `Tweet エンドポイント` {
+    @Test
+    @DataSet(value = ["datasets/setup/1-token.yml"])
+    @ExpectedDataSet(value = ["datasets/expected/1-token.yml"])
+    fun `yesterday シリーズを投稿できる`() {
+      // setup
+      stubFor(post(urlEqualTo("/media/upload.json"))
+        .willReturn(aResponse()
+          .withStatus(201)
+          .withHeader("Content-Type", "application/json; charset=utf-8")
+          .withBody("{\"media_id\":710511363345354753,\"media_id_string\":\"710511363345354753\",\"size\":11065,\"expires_after_secs\":86400,\"image\":{\"image_type\":\"image/jpeg\",\"w\":800,\"h\":320}}")))
+
+      stubFor(post(urlEqualTo("/tweets"))
+        .withHeader("Authorization", matching("^OAuth oauth_consumer_key=\"api-key-dummy\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"[^\"]+\",oauth_nonce=\"[^\"]+\",oauth_version=\"1\\.0\",oauth_token=\"oauth1-access-token-dummy\",oauth_signature=\"[^\"]+\"\$"))
+        .withHeader("Content-Type", equalTo("application/json"))
+        .withRequestBody(equalTo("{\"media\":{\"media_ids\":[\"710511363345354753\",\"710511363345354753\",\"710511363345354753\",\"710511363345354753\"]},\"text\":\"yesterday\"}"))
+        .willReturn(aResponse()
+          .withStatus(201)
+          .withHeader("Content-Type", "application/json; charset=utf-8")
+          .withBody("{\"data\":{\"edit_history_tweet_ids\":[\"1778064547540353187\"],\"id\":\"1778064547540353187\",\"text\":\"tweet\"}}")))
+
+      // execute & assert
+      given()
+        .post("/v1/tweet/yesterday")
+        .then()
+        .statusCode(HttpStatus.CREATED.value())
+        .body("tweetId", Matchers.equalTo("1778064547540353187"))
     }
   }
 }
