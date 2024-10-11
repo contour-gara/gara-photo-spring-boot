6f3321045f46e08ea4554c54ab0169918302bf54
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-10-12 07:21:17 +0900

    detekt のため kotlin はあげない

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.spring") version "2.0.21"
+    kotlin("jvm") version "2.0.10"
+    kotlin("plugin.spring") version "2.0.10"
     id("org.sonarqube") version "5.1.0.4882"
     id("jacoco")
     id("io.gitlab.arturbosch.detekt") version "1.23.7"
