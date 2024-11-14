961beaefe65781a26c225e10d976356197277058
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-11-15 04:39:29 +0900

    過剰なスコープを修正

--- konsist-test/build.gradle.kts
+++ konsist-test/build.gradle.kts
@@ -36,7 +36,7 @@ dependencyManagement {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
