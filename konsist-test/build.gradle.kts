8301eab0e8e3fd6b193f1c3f965eda5adda59f3c
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-12-04 01:00:09 +0900

    update konsist
    https://github.com/LemonAppDev/konsist/releases/tag/v0.17.1

--- konsist-test/build.gradle.kts
+++ konsist-test/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("io.kotest:kotest-framework-datatest:5.9.1")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
-    testImplementation("com.lemonappdev:konsist:0.17.0")
+    testImplementation("com.lemonappdev:konsist:0.17.1")
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.7")
 }
 
