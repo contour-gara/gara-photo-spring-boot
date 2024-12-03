30edb86a8488f44192f18a453e9597d62ad4df39
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-12-04 01:58:36 +0900

    assertj-db アップデートによる修正
    https://github.com/assertj/assertj-db/releases/tag/assertj-db-3.0.0

--- gara-photo-app-server/src/test/kotlin/org/contourgara/garaphotospringboot/infrastructure/TokenRepositoryImplTest.kt
+++ gara-photo-app-server/src/test/kotlin/org/contourgara/garaphotospringboot/infrastructure/TokenRepositoryImplTest.kt
@@ -9,10 +9,10 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import org.assertj.db.api.Assertions.*
-import org.assertj.db.type.Changes
+import org.assertj.db.type.AssertDbConnection
+import org.assertj.db.type.AssertDbConnectionFactory
 import org.assertj.db.type.DateTimeValue
 import org.assertj.db.type.DateValue
-import org.assertj.db.type.Table
 import org.assertj.db.type.TimeValue
 import org.contourgara.garaphotospringboot.domain.Token
 import org.contourgara.garaphotospringboot.domain.infrastructure.TokenRepository
@@ -26,160 +26,179 @@ import java.time.LocalDateTime
 class TokenRepositoryImplTest(
     private val sut: TokenRepository,
     private val dataSource: HikariDataSource,
-) : WordSpec({
-    beforeEach {
-        dbSetup(dataSource) {
-            deleteAllFrom("token")
-        }.launch()
-    }
+) : WordSpec() {
+    lateinit var assertDbConnection: AssertDbConnection
 
-    "保存" When {
-        "レコードに重複がない" should {
-            "トークン情報をテーブルに保存できる" {
-                // setup
-                val changes = Changes(Table(dataSource, "token"))
+    init {
+        beforeSpec {
+            assertDbConnection = AssertDbConnectionFactory.of(
+                "jdbc:h2:mem:test;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false", "sa", "sa"
+            ).create()
+        }
+        beforeEach {
+            dbSetup(dataSource) {
+                deleteAllFrom("token")
+            }.launch()
+        }
 
-                // execute
-                changes.setStartPointNow()
+        "保存" When {
+            "レコードに重複がない" should {
+                "トークン情報をテーブルに保存できる" {
+                    // setup
+                    val changes = assertDbConnection.changes()
+                        .tables(assertDbConnection.table("token").build())
+                        .build()
 
-                sut.insert(
-                    Token(
+                    // execute
+                    changes.setStartPointNow()
+
+                    sut.insert(
+                        Token(
+                            "accessToken",
+                            "refreshToken",
+                            "clientId",
+                            LocalDateTime.of(2024, 4, 14, 19, 48, 34)
+                        )
+                    )
+
+                    changes.setEndPointNow()
+
+                    // assert
+                    assertThat(changes)
+                        .hasNumberOfChanges(1)
+                        .change()
+                        .isCreation()
+                        .rowAtEndPoint()
+                        .value("id").isEqualTo("1")
+                        .value("access_token").isEqualTo("accessToken")
+                        .value("refresh_Token").isEqualTo("refreshToken")
+                        .value("date_time").isEqualTo(
+                            DateTimeValue.of(DateValue.of(2024, 4, 14), TimeValue.of(19, 48, 34))
+                        )
+                }
+            }
+
+            "レコードに重複がある" should {
+                "例外が返る" {
+                    // setup
+                    val changes = assertDbConnection.changes()
+                        .tables(assertDbConnection.table("token").build())
+                        .build()
+
+                    dbSetup(dataSource) {
+                        insertInto("token") {
+                            columns("id", "access_token", "refresh_token", "date_time")
+                            values("1", "accessToken", "refreshToken", LocalDateTime.of(2024, 4, 14, 19, 48, 34))
+                        }
+                    }.launch()
+
+                    // execute & assert
+                    changes.setStartPointNow()
+
+                    shouldThrowExactly<DuplicateKeyException> {
+                        sut.insert(Token("accessToken", "refreshToken", "clientId", LocalDateTime.now()))
+                    }
+
+                    changes.setEndPointNow()
+
+                    assertThat(changes).hasNumberOfChanges(0)
+                }
+            }
+        }
+
+        "取得" When {
+            "レコードあり" should {
+                "トークン情報を取得できる" {
+                    // setup
+                    val changes = assertDbConnection.changes()
+                        .tables(assertDbConnection.table("token").build())
+                        .build()
+
+                    dbSetup(dataSource) {
+                        insertInto("token") {
+                            columns("id", "access_token", "refresh_token", "date_time")
+                            values("1", "accessToken", "refreshToken", "2024-04-14 19:48:34")
+                        }
+                    }.launch()
+
+                    // execute & assert
+                    changes.setStartPointNow()
+
+                    sut.find("clientId") shouldBe Token(
                         "accessToken",
                         "refreshToken",
                         "clientId",
                         LocalDateTime.of(2024, 4, 14, 19, 48, 34)
                     )
-                )
 
-                changes.setEndPointNow()
+                    changes.setEndPointNow()
 
-                // assert
-                assertThat(changes)
-                    .hasNumberOfChanges(1)
-                    .change()
-                    .isCreation()
-                    .rowAtEndPoint()
-                    .value("id").isEqualTo("1")
-                    .value("access_token").isEqualTo("accessToken")
-                    .value("refresh_Token").isEqualTo("refreshToken")
-                    .value("date_time").isEqualTo(
-                        DateTimeValue.of(DateValue.of(2024, 4, 14), TimeValue.of(19, 48, 34))
-                    )
-            }
-        }
-
-        "レコードに重複がある" should {
-            "例外が返る" {
-                // setup
-                val changes = Changes(Table(dataSource, "token"))
-
-                dbSetup(dataSource) {
-                    insertInto("token") {
-                        columns("id", "access_token", "refresh_token", "date_time")
-                        values("1", "accessToken", "refreshToken", LocalDateTime.of(2024, 4, 14, 19, 48, 34))
-                    }
-                }.launch()
-
-                // execute & assert
-                changes.setStartPointNow()
-
-                shouldThrowExactly<DuplicateKeyException> {
-                    sut.insert(Token("accessToken", "refreshToken", "clientId", LocalDateTime.now()))
+                    assertThat(changes).hasNumberOfChanges(0)
                 }
-
-                changes.setEndPointNow()
-
-                assertThat(changes).hasNumberOfChanges(0)
             }
-        }
-    }
 
-    "取得" When {
-        "レコードあり" should {
-            "トークン情報を取得できる" {
-                // setup
-                val changes = Changes(Table(dataSource, "token"))
+            "レコードなし" should {
+                "null が返る" {
+                    // setup
+                    val changes = assertDbConnection.changes()
+                        .tables(assertDbConnection.table("token").build())
+                        .build()
 
-                dbSetup(dataSource) {
-                    insertInto("token") {
-                        columns("id", "access_token", "refresh_token", "date_time")
-                        values("1", "accessToken", "refreshToken", "2024-04-14 19:48:34")
-                    }
-                }.launch()
+                    // execute & assert
+                    changes.setStartPointNow()
 
-                // execute & assert
-                changes.setStartPointNow()
+                    sut.find("clientId").shouldBeNull()
 
-                sut.find("clientId") shouldBe Token(
-                    "accessToken",
-                    "refreshToken",
-                    "clientId",
-                    LocalDateTime.of(2024, 4, 14, 19, 48, 34)
-                )
+                    changes.setEndPointNow()
 
-                changes.setEndPointNow()
-
-                assertThat(changes).hasNumberOfChanges(0)
+                    assertThat(changes).hasNumberOfChanges(0)
+                }
             }
         }
 
-        "レコードなし" should {
-            "null が返る" {
-                // setup
-                val changes = Changes(Table(dataSource, "token"))
+        "更新" When {
+            "レコードがあある" should {
+                "トークン情報を更新できる" {
+                    // setup
+                    val changes = assertDbConnection.changes()
+                        .tables(assertDbConnection.table("token").build())
+                        .build()
 
-                // execute & assert
-                changes.setStartPointNow()
+                    dbSetup(dataSource) {
+                        insertInto("token") {
+                            columns("id", "access_token", "refresh_token", "date_time")
+                            values("1", "accessToken", "refreshToken", "2024-04-14 19:48:34")
+                        }
+                    }.launch()
 
-                sut.find("clientId").shouldBeNull()
+                    // execute
+                    changes.setStartPointNow()
 
-                changes.setEndPointNow()
-
-                assertThat(changes).hasNumberOfChanges(0)
-            }
-        }
-    }
-
-    "更新" When {
-        "レコードがあある" should {
-            "トークン情報を更新できる" {
-                // setup
-                val changes = Changes(Table(dataSource, "token"))
-
-                dbSetup(dataSource) {
-                    insertInto("token") {
-                        columns("id", "access_token", "refresh_token", "date_time")
-                        values("1", "accessToken", "refreshToken", "2024-04-14 19:48:34")
-                    }
-                }.launch()
-
-                // execute
-                changes.setStartPointNow()
-
-                sut.update(
-                    Token(
-                        "accessToken2",
-                        "refreshToken2",
-                        "clientId",
-                        LocalDateTime.of(2024, 4, 14, 19, 48, 34)
+                    sut.update(
+                        Token(
+                            "accessToken2",
+                            "refreshToken2",
+                            "clientId",
+                            LocalDateTime.of(2024, 4, 14, 19, 48, 34)
+                        )
                     )
-                )
 
-                changes.setEndPointNow()
+                    changes.setEndPointNow()
 
-                // assert
-                assertThat(changes)
-                    .hasNumberOfChanges(1)
-                    .change()
-                    .isModification()
-                    .rowAtEndPoint()
-                    .value("id").isEqualTo("1")
-                    .value("access_token").isEqualTo("accessToken2")
-                    .value("refresh_Token").isEqualTo("refreshToken2")
-                    .value("date_time").isEqualTo(
-                        DateTimeValue.of(DateValue.of(2024, 4, 14), TimeValue.of(19, 48, 34))
-                    )
+                    // assert
+                    assertThat(changes)
+                        .hasNumberOfChanges(1)
+                        .change()
+                        .isModification()
+                        .rowAtEndPoint()
+                        .value("id").isEqualTo("1")
+                        .value("access_token").isEqualTo("accessToken2")
+                        .value("refresh_Token").isEqualTo("refreshToken2")
+                        .value("date_time").isEqualTo(
+                            DateTimeValue.of(DateValue.of(2024, 4, 14), TimeValue.of(19, 48, 34))
+                        )
+                }
             }
         }
     }
-})
+}
